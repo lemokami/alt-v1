a343d8fb86fe238ad02bfadc14bc4639b8159e63
Jess <jesswinw@gmail.com>
2023-12-17 13:49:33 +0530

    refactor: seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
-import compress from "astro-compress";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()]
+  site: 'https://www.jesswin.dev',
+  integrations: [tailwind(), compress(), sitemap()],
 });
